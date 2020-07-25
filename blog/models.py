b37a0f8ce4ab900d4789f862426c61c4353daa44
Vernit <vernit.jain1@gmail.com>
2020-07-25 17:20:34 +0530

    Change the view of blog

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
+    images = models.ImageField(upload_to='blog/images/', null=True)
 
     def __str__(self):
         return self.title
