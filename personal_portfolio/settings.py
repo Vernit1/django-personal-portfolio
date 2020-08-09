f49da8cc7f9a420f9854d2e58f6caaedb28795e1
Vernit1 <vernit.jain1@gmail.com>
2020-08-09 19:35:36 +0530

    check

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,5 +138,3 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = "skwgkeodygdidbar"
-
-django_heroku.settings(locals())
